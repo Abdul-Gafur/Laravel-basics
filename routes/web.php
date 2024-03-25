a889071e86131f082e6fc1082124d9cba566c6d8
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-03-25 09:40:37 +0000

    modified

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::get('/pizzers/{pizzer_name?}', function (string $pizzer_name = null) {
 // Regular expression constraints
 Route::get('user/{user_id}/{user_name}', function (string $user_id, string $user_name) {
     return view('pizzers', ['user_id' => $user_id, 'user_name' => $user_name]);
-})->whereNumber('$user_id')->whereAlpha('$user_name');
+})->whereNumber('user_id')->whereAlpha('user_name'); // Returns 404 error if constraints is not met
